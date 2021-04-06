78647d743fad77b169d43ea7dd722947a28ebb15
meitao87 <meitao87@outlook.com>
2021-04-06 22:12:51 +0200

    remove unsed models, renew the datagenerator

--- face_recognition/model.py
+++ face_recognition/model.py
@@ -10,64 +10,9 @@ from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.applications.resnet50 import ResNet50
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
 from tensorflow.keras.layers import Conv2D, Activation, MaxPooling2D, Flatten, Dense, concatenate, Concatenate, InputLayer, Dropout, Reshape, GlobalMaxPooling2D, GlobalAveragePooling2D
-# from tensorflow import keras
-
-
-class Face_loc:
-    def __init__(self, width, height, depth, classes):
-        # width and height have to be 2**n
-        self.width = width
-        self.height = height
-        self.depth = depth
-        self.classes = classes
-
-    def build(self):
-        model = tf.keras.Sequential()
-        inputShape = (self.height, self.width, self.depth)
-
-        # first set of CONV => CONV => RELU => POOL layers
-        model.add(Conv2D(64, (3, 3), padding="same",
-            input_shape=inputShape))
-        model.add(Activation("relu"))
-        # model.add(Conv2D(32, (3, 3), padding="same"))
-        # model.add(Activation("relu"))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-
-        # second set of CONV => CONV => RELU => POOL layers
-        model.add(Conv2D(128, (3, 3), padding="same"))
-        model.add(Activation("relu"))
-        # model.add(Conv2D(64, (3, 3), padding="same"))
-        # model.add(Activation("relu"))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-
-        # first set of FC => RELU layers
-        # model.add(Flatten())
-        # model.add(Dense(64))
-        model.add(Conv2D(64, (1, 1), padding="same"))
-        model.add(Activation("relu"))
-        model.add(Dropout(0.5))
-
-        # second set of FC => RELU layers
-        # model.add(Dense(64))
-        model.add(Conv2D(64, (1, 1), padding="same"))
-        model.add(Activation("relu"))
-        model.add(Dropout(0.5))
-
-        # softmax classifier
-        # model.add(Dense(self.classes))
-        model.add(Conv2D(self.classes, (int(self.height/4), int(self.width/4))))
-        model.add(Activation("relu"))
-
-        model.add(Flatten())
-        model.add(Dense(self.classes))
-        model.add(Activation("softmax"))
-        # return the constructed network architecture
-        print(model.summary())
-        return model
 
 
 class DataGenerator(tf.keras.utils.Sequence):
-    
     @staticmethod
     def json_to_bbox(json_file_path):
         with open (json_file_path) as f:
@@ -82,24 +27,34 @@ class DataGenerator(tf.keras.utils.Sequence):
         return x, y, w, h
 
     def __init__(self, training_path, S=7, B=2, batch_size=32, dim=(48*3, 64*3), n_channels=3,
-                 n_classes=1, shuffle=True):
+                 shuffle=True):
         self.dim = dim
         self.H, self.W = self.dim[0], self.dim[1]
         self.batch_size = batch_size
         self.S = S
         self.B = B
         self.training_path = training_path
+        self.sub_dirs = os.listdir(training_path)
+        le = LabelEncoder()
+        le.fit(self.sub_dirs)
+        # self.label_name = le.classes_
+        self.n_classes = len(self.sub_dirs)
         self.n_channels = n_channels
-        self.n_classes = n_classes
         self.shuffle = shuffle
         self.img_path = []
         self.label_path = []
-        for subdir, dirs, files in os.walk(self.training_path):
-            for f in files:
-                if(os.path.join(subdir, f)).endswith(".jpg"):
-                    label = f[:-4] + '.json'
-                    self.img_path.append(os.path.join(subdir, f)) 
-                    self.label_path.append(os.path.join(subdir, label))
+        for self.sub_dir in self.sub_dirs:
+            for f in (os.listdir(os.path.join(self.training_path, self.sub_dir))):
+                if f.endswith('jpg'): # iterate over all images
+                    # img_path contains all images
+                    self.img_path.append(os.path.join(self.training_path, self.sub_dir, f))
+                    # generate OneHotLabel for this sub folder
+                    self.label_encoder = le.transform(self.sub_dir.split(" "))
+                    self.OneHotLabel = tf.keras.utils.to_categorical(self.label_encoder[0], num_classes=self.n_classes)
+                    print('self.OneHotLabel', self.OneHotLabel, (self.OneHotLabel.shape))
+                    # check if the json file exists, yes: clss_id, xywhpc, xywhpc; (no: class_id, 00000) makes no sense...
+                    self.json_path = os.path.join(self.training_path,self.sub_dir, f[:-4] + '.json')
+                    self.label_path.append([self.json_path, self.OneHotLabel])
         # using assert?
         ############### rebuild the labels structure
         self.training = np.empty((len(self.img_path), self.H, self.W, self.n_channels))
@@ -111,19 +66,18 @@ class DataGenerator(tf.keras.utils.Sequence):
             # convert gray scale to RGB
             img  = np.stack((img,)*3, axis=-1)
             self.training[i] = img
-            # if label exists, read the label, and make the first entry 1 (means there is face in the image)
-            if os.path.isfile(self.label_path[i]):
-                x, y, w, h = DataGenerator.json_to_bbox(self.label_path[i])
-                # from website
-                loc = [self.S * x, self.S * y]
-                loc_i = int(loc[1])
-                loc_j = int(loc[0])
-                y = loc[1] - loc_i
-                x = loc[0] - loc_j
-                # label is like : class_id, (x,y,w,h,conf), (x,y,w,h,conf), (x,y,w,h,conf)...
-                self.labels[i, loc_i, loc_j, 0] = 1
-                self.labels[i, loc_i, loc_j, self.n_classes: self.n_classes+4] = [x, y, w, h]
-                self.labels[i, loc_i, loc_j, self.n_classes+4] = 1                
+            #  read the label, and make the first entry 1 (means there is face in the image)
+            x, y, w, h = DataGenerator.json_to_bbox(self.label_path[i][0])
+            # from website
+            loc = [self.S * x, self.S * y]
+            loc_i = int(loc[1])
+            loc_j = int(loc[0])
+            y = loc[1] - loc_i
+            x = loc[0] - loc_j
+            # label is like : class_id, (x,y,w,h,conf), (x,y,w,h,conf), (x,y,w,h,conf)...
+            self.labels[i, loc_i, loc_j, :self.n_classes] = self.label_path[i][1]
+            self.labels[i, loc_i, loc_j, self.n_classes: self.n_classes+4] = [x, y, w, h]
+            self.labels[i, loc_i, loc_j, self.n_classes+4] = 1                
             
         self.on_epoch_end()
 
@@ -169,55 +123,6 @@ class DataGenerator(tf.keras.utils.Sequence):
         return X, y
 
 
-class Face_classify:
-    def __init__(self, width, height, depth, classes):
-        # width and height have to be 2**n
-        self.width = width
-        self.height = height
-        self.depth = depth
-        self.classes = classes
-
-    def build(self):
-        model = tf.keras.Sequential()
-        inputShape = (self.height, self.width, self.depth)
-
-        # first set of CONV => CONV => RELU => POOL layers
-        model.add(Conv2D(64, (3, 3), padding="same",
-            input_shape=inputShape))
-        model.add(Activation("relu"))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-
-        # second set of CONV => CONV => RELU => POOL layers
-        model.add(Conv2D(128, (3, 3), padding="same"))
-        model.add(Activation("relu"))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-
-        # first set of FC => RELU layers
-        # model.add(Flatten())
-        # model.add(Dense(64))
-        model.add(Conv2D(64, (1, 1), padding="same"))
-        model.add(Activation("relu"))
-        model.add(Dropout(0.5))
-
-        # second set of FC => RELU layers
-        # model.add(Dense(64))
-        model.add(Conv2D(64, (1, 1), padding="same"))
-        model.add(Activation("relu"))
-        model.add(Dropout(0.5))
-
-        # softmax classifier
-        # model.add(Dense(self.classes))
-        model.add(Conv2D(self.classes, (int(self.height/4), int(self.width/4))))
-        model.add(Activation("relu"))
-
-        model.add(Flatten())
-        model.add(Dense(self.classes))
-        model.add(Activation("softmax"))
-        # return the constructed network architecture
-        print(model.summary())
-        return model
-
-
 # https://www.maskaravivek.com/post/yolov1/
 # inspried by github repo: https://github.com/JY-112553/yolov1-keras-voc
 class Yolo_Reshape(tf.keras.layers.Layer):
@@ -255,69 +160,12 @@ class Yolo_Reshape(tf.keras.layers.Layer):
         return outputs    
 
 
-class Face_yolo:
-    def __init__(self, B=2, C=1, S=7, img_w=192, img_h=144):
-        self.B = B
-        self.S = S
-        # self.cell_w = cell_w
-        # self.cell_h = cell_h
-        self.C = C
-        self.img_w = img_w
-        self.img_h = img_h
-   
-    def build(self):
-        lrelu = tf.keras.layers.LeakyReLU(alpha=0.1)
-        model = Sequential()
-        model.add(Conv2D(filters=32, kernel_size= (7, 7), strides=(1, 1), input_shape =(self.img_h, self.img_w, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding = 'same'))
-
-        model.add(Conv2D(filters=64, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding = 'same'))
-
-        model.add(Conv2D(filters=64, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        model.add(Conv2D(filters=64, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        model.add(Conv2D(filters=64, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        # model.add(Conv2D(filters=512, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding = 'same'))
-
-        model.add(Conv2D(filters=64, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        model.add(Conv2D(filters=64, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        # model.add(Conv2D(filters=256, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        # model.add(Conv2D(filters=512, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        # model.add(Conv2D(filters=256, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        # model.add(Conv2D(filters=512, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        # model.add(Conv2D(filters=256, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        # model.add(Conv2D(filters=512, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        # model.add(Conv2D(filters=512, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        # model.add(Conv2D(filters=1024, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding = 'same'))
-
-        model.add(Conv2D(filters=64, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        # model.add(Conv2D(filters=1024, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        # model.add(Conv2D(filters=512, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        # model.add(Conv2D(filters=1024, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        # model.add(Conv2D(filters=1024, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
-        # model.add(Conv2D(filters=1024, kernel_size= (3, 3), strides=(2, 2), padding = 'same'))
-
-        # model.add(Conv2D(filters=1024, kernel_size= (3, 3), activation=lrelu, kernel_regularizer=l2(5e-4)))
-        model.add(Conv2D(filters=128, kernel_size= (3, 3), activation=lrelu, kernel_regularizer=l2(5e-4)))
-
-        model.add(Flatten())
-        model.add(Dense(64))
-        model.add(Dense(128))
-        model.add(Dropout(0.5))
-        model.add(Dense(self.S * self.S * (self.B * 5 + self.C), activation='sigmoid'))
-        model.add(Yolo_Reshape(S=self.S, B=self.B, C=self.C))
-        model.summary()
-        return model
-
-
 # Using pretrained ResNet50 as feather extraction
 class Face_yolo_Resnet:
     def __init__(self, B=2, C=1, S=7, img_w=192, img_h=144):
-        self.B = B
-        self.S = S
-        self.C = C
+        self.B = B # bbox number
+        self.S = S # grid number
+        self.C = C # class number
         self.img_w = img_w
         self.img_h = img_h
    
@@ -339,7 +187,7 @@ class Face_yolo_Resnet:
 
 
 # image classifier with pretrained MobileNet (mobilenet is quick and small)
-class Face_classifier_mobilenet:
+'''class Face_classifier_mobilenet:
     def __init__(self, n_classes, input_shape):
         self.n_classes = n_classes
         self.input_shape = input_shape
@@ -354,12 +202,12 @@ class Face_classifier_mobilenet:
         for layer in base_model.layers:
             layer.trainable = False
         model.summary()
-        return model
+        return model'''
 
 
-class DataGenerator_classsifier(tf.keras.utils.Sequence):
+'''class DataGenerator_classsifier(tf.keras.utils.Sequence):
 
-    def __init__(self, training_path, batch_size=32, H=144, W=192, n_classes=3, n_channels=3, shuffle=True):
+    def __init__(self, training_path, batch_size=32, H=144, W=192, n_channels=3, shuffle=True):
         self.H = H
         self.W = W
         self.n_channels = n_channels
@@ -380,7 +228,6 @@ class DataGenerator_classsifier(tf.keras.utils.Sequence):
                     self.label_encoder = le.transform(self.sub_dir.split(" "))
                     self.OneHotLabel = tf.keras.utils.to_categorical(self.label_encoder, num_classes=self.n_classes)
                     self.label_path.append(self.OneHotLabel)
-                    # print(self.OneHotLabel)
         self.training = np.empty((len(self.img_path), self.H, self.W, self.n_channels))
         self.labels = np.zeros((len(self.img_path), self.n_classes)) 
         for i in range(len(self.img_path)):
@@ -432,4 +279,4 @@ class DataGenerator_classsifier(tf.keras.utils.Sequence):
             X[i,] = training_img
             # Store class
             y[i] = training_label
-        return X, y
+        return X, y'''
